db03b96c20ba2b70781688b7d04fb86ff1d6d3b8
Eli T. Drumm <eli@eli.td>
2023-04-15 13:04:45 -0400

    adding nesting example

---
+++ unit3/unit3-part1/nesting-example.js
@@ -0,0 +1,17 @@
+const nested = {
+  dates: [
+    {
+      date: [
+        [
+          {
+            obj: {
+              num: 7
+            }
+          }
+        ]
+      ]
+    }
+  ]
+}
+
+nested.arr[0].arr[0][0].obj.num
