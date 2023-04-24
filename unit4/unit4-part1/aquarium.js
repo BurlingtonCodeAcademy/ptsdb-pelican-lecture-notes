5359f2becce70644d61577e0c214128ef449593c
Eli T. Drumm <eli@eli.td>
2023-04-24 19:29:03 -0400

    added unit 4 content (basic DOM manip)

---
+++ unit4/unit4-part1/aquarium.js
@@ -0,0 +1,38 @@
+
+console.log('is this attached?')
+
+const ourEels = {}
+
+const aquaBox = document.getElementById('aqua-box')
+// const eelName = document.getElementById('eel-name')
+const eelName = document.querySelector('#eel-name')
+
+function addNewEel() {
+  const newEel = document.createElement('img')
+  newEel.src = 'eel.jpg'
+  newEel.style.maxWidth = '20rem'
+  aquaBox.appendChild(newEel)
+
+  const name = eelName.value
+  ourEels[name] = newEel
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = `Click here to delete ${name}`
+  deleteButton.addEventListener('click', () => {
+    aquaBox.removeChild(newEel)
+    delete ourEels[name]
+    document.body.removeChild(deleteButton)
+  })
+  document.body.appendChild(deleteButton)
+}
+
+// eelAdder is a button that, uh, adds eels
+const eelAdder = document.getElementById('eel-adder')
+eelAdder.addEventListener('click', addNewEel)
+
+const eelCensus = document.getElementById('eel-census')
+eelCensus.addEventListener ('click', (e) => {
+  console.log(`There are ${aquaBox.children.length} eels in the aquarium.`)
+})
+
+
---
+++ unit4/unit4-part1/iceberg.js
@@ -0,0 +1,4 @@
+// runs in the browser
+// console.logs will appear there
+
+const mainEel = document.getElementById('main-eel')
