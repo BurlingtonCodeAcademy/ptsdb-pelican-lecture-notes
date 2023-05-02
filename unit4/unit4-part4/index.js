1f1735f52e027ed2b82cc45970fdf8bf88ab70fd
Eli T. Drumm <eli@eli.td>
2023-05-01 20:08:10 -0400

    added unit4-part4

---
+++ unit4/unit4-part4/index.js
@@ -0,0 +1,13 @@
+
+const {
+  message,
+  three
+} = require('./numchecker')
+
+const { a } = { a: 1, b: 2 }
+
+
+
+console.log(message)
+
+console.log(a)
---
+++ unit4/unit4-part4/numchecker.js
@@ -0,0 +1,6 @@
+const isEven = require('is-even')
+
+module.exports = {
+  message: `Is 5 even? ${isEven(5)}`,
+  three: 3
+}
